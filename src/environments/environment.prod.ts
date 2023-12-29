ea2346da62ec9200379e5c796cbd9d1304b42fc7
William Nascimento <william.nascimento2010@gmail.com>
2023-12-29 09:21:58 -0300

    alterando para https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  URL_API: 'http://161.35.255.131:32100',
+  URL_API: 'http://bella-skin.api-danweb-softwares.click',
   hmr: false,
   role: ''
-  
 };
